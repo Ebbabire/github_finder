6bd16f2b44faf38099e6fd2ff256851ffceb2f87
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-02-17 19:09:17 +0300

    about componet

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h1 className="text-6xl text-black mb-4 px-6">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light text-black px-6">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-black px-6">
+        Version <span className="text-green-500">1.0.0</span>
+      </p>
+      <p className="text-lg text-black px-6">
+        Layout By:
+        <a className="text-green-500" href="#">
+          Hassib Moddasser
+        </a>
+      </p>
+    </>
+  );
 };
 
 export default About;
