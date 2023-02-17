5a0aa642226ccf5b12132821d62cc558fdfc6652
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-02-17 14:08:30 +0300

    added tailwind css and daisyui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-purple-500 h-10">
+      <h1 className="text-white text-2xl">Hello World</h1>
+      <button className="btn">click</button>
     </div>
   );
-}
+};
 
 export default App;
