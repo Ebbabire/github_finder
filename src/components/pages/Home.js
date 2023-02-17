f041f2cc9e24ceebee994d51879eb94abaeba605
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-02-17 19:09:40 +0300

    added home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import SearchBar from '../SearchBar';
+import UsersList from './UsersList';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto mb-12">
+      <SearchBar />
+      <UsersList />
+    </div>
+  );
 };
 
 export default Home;
