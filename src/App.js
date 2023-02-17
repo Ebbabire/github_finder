913afa3834abe8d4bddfe44aee5a93427454baa5
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-02-17 15:56:25 +0300

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import NavBar from './components/layout/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import NotFound from './components/pages/NotFound';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
