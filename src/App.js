11723853fcd030a8329ea40bfa8e85f5db6fe31a
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-02-28 14:47:19 +0300

    added the context provider

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import NotFound from './components/pages/NotFound';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Footer from './components/layout/Footer';
+import { UserContextProvider } from './store/UserContext';
+
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col justify-between  bg-[#FCEDDA]">
-        <NavBar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <UserContextProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen overflow-y-auto bg-[#FCEDDA]">
+          <NavBar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </UserContextProvider>
   );
 };
 
